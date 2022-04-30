ca2f6b50d8ccf982a1f3bf5af99ac97efc2ce093
omarjabloun-hub <omar.jabloun@insat.u-carthage.tn>
2022-04-30 15:03:03 +0100

    Desktop First Controller commit

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'app_first')]
+    public function index(): Response
+    {
+
+        return $this->render('./templates/first/index.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+}
